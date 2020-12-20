cc12e72efdad54e2ca505e135f6284c7a36d934b
JohanssonXXII <69969647+JohanssonXXII@users.noreply.github.com>
2020-12-20 20:14:54 +0100

    Ripristino codice precedente
    Return ai vecchi dati
    
    Co-Authored-By: TommasoFachin <69682170+TommasoFachin@users.noreply.github.com>

--- Elezioni v2/src/elezioni/v2/ElezioniV2.java
+++ Elezioni v2/src/elezioni/v2/ElezioniV2.java
@@ -12,32 +12,41 @@ import java.util.Scanner;
 public class ElezioniV2 {
     
     
+    
+    
+        
+        
+    
+    
+    
+        
+    
+    
     public static void main(String[] args) throws IOException {
         
         
         Scanner in = new Scanner(System.in);
-        int nvotanti;
-        ArrayList<Votante> listaVotanti = new ArrayList<Votante>();
+        ArrayList <Votante> listaVotanti = new ArrayList<Votante>();
         ArrayList<Candidato>listaCandidati= new ArrayList<Candidato>();
-       void Inserimento_Votanti(listaVotanti){
-        
+        //INIZIO AGGIUNTA VOTANTI ALL'ARRAYLIST
         System.out.println("Inserisci il numero di votanti");
-        nvotanti = in.nextInt();
+        int nvotanti = in.nextInt();
         for(int i=0;i<nvotanti;i++){
             System.out.println("Inserisci votante\netà , email e pin");
         int eta = in.nextInt();
         String email = in.next();
         int pin = in.nextInt();
-        
         listaVotanti.add(new Votante(eta,email,pin));
-        }
-        
-        }
+        //FINE AGGIUNTA VOTANTI ALL'ARRAY LIST
         
         
         
+        
+        
+        
+        //INIZIO SCRITTURA SU FILE TXT DEI CANDITATI E VOTANTI
         Salvataggio.stampaSuFile(listaCandidati, listaVotanti);
-        
+        //FINE SCRITTURA SU FILE TXT DEI CANDITATI E VOTANTI
         
         
         //Salvataggio s = new Salvataggio();
@@ -47,5 +56,8 @@ public class ElezioniV2 {
         
         
     }
+
     
+    
+}
 }
