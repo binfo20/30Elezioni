c9c015e5162d3d9fa6eb895f9ee7ba09d6ab4494
JohanssonXXII <69969647+JohanssonXXII@users.noreply.github.com>
2020-12-21 16:11:25 +0100

    Finale Codice
    Direi che siamo a buon punto col codice , almost finito.

--- Elezioni v2/src/elezioni/v2/ElezioniV2.java
+++ Elezioni v2/src/elezioni/v2/ElezioniV2.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.Scanner;
 
+
 /**
  *
  * @author Aimen
@@ -12,12 +13,24 @@ import java.util.Scanner;
 public class ElezioniV2 {
     
     
+    public static void RisultatiFinali(ArrayList<Voto> listaVoti) 
+    { 
+  
+        // hash set is created and elements of 
+        // arraylist are insertd into it 
+        Set<Voto> st = new HashSet<Voto>(listaVoti); 
+        for (Voto s : st) 
+            System.out.println(s + ": " + Collections.frequency(listaVoti, s)); 
+    } 
+    
+    
     public static void main(String[] args) throws IOException {
         
         
         Scanner in = new Scanner(System.in);
         ArrayList <Votante> listaVotanti = new ArrayList<Votante>();
         ArrayList<Candidato>listaCandidati= new ArrayList<Candidato>();
+        ArrayList<Voto>listaVoti= new ArrayList<Voto>();
         //INIZIO AGGIUNTA VOTANTI ALL'ARRAYLIST
         System.out.println("Inserisci il numero di votanti");
         int nvotanti = in.nextInt();
@@ -34,7 +47,7 @@ public class ElezioniV2 {
            System.out.println("Inserisci il numero di candidati alle elezioni");
             int nCandidati = in.nextInt();
           for(int j=0;j<nCandidati;j++){
-                System.out.println("Inserisci candidato\n Nome:");
+                System.out.println("Inserisci candidato\nNome:");
                  String nome = in.next();
                   System.out.println("Cognome:");
                  String cognome = in.next();
@@ -45,6 +58,23 @@ public class ElezioniV2 {
                     listaCandidati.add(new Candidato(nome,cognome,età,Partito));
           }
         //FINE AGGIUNTA CANDIDATI ALL'ARRAYLIST
+        
+        //INIZIO INSERIMENTI VOTO
+        System.out.println("Caro votante , i candidati sono : ");
+        for(int k=0;k<listaCandidati.size();k++){
+            
+            System.out.println(listaCandidati.get(k));
+        }
+            for(int l=0;l<listaVotanti.size();l++){
+                System.out.println("\n\n\n\n\n\n"+listaVotanti.get(l)+" è il suo turno\nDeve indicare il nome del partita scrivendolo correttamente");
+                String voto = in.next();
+                listaVoti.add(new Voto(voto));
+            }
+            
+        //FINE INSERIMENTO VOTI
+        //MI MOSTRA I RISULTATI FINALI A SCHERMO
+        RisultatiFinali(listaVoti);
+        
         
         
         
--- Elezioni v2/src/elezioni/v2/Voto.java
+++ Elezioni v2/src/elezioni/v2/Voto.java
@@ -10,5 +10,31 @@ package elezioni.v2;
  * @author Aimen
  */
 public class Voto {
+   
+    String voto;
+
+    public Voto(String voto) {
+        this.voto = voto;
+    }
+    
+    
+    public String getVoto() {
+        return voto;
+    }
+
+    public void setVoto(String voto) {
+        this.voto = voto;
+    }
+
+    @Override
+    public String toString() {
+        return "Voto{" + "voto=" + voto + '}';
+    }
+
+    
+
+    
+    
+    
     
 }
