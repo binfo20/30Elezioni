8f1023fb617be0225aab7f61c639bfd7b12161f2
Maurizio Morselli <morsellimaurizio@istitutocorni.it>
2021-01-06 18:56:24 +0100

    Menu FX

--- Elezioni_JavaFX/src/elezioni_javafx/MenuController.java
+++ Elezioni_JavaFX/src/elezioni_javafx/MenuController.java
@@ -7,16 +7,41 @@ package elezioni_javafx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
  * @author famig
  */
 public class MenuController implements Initializable {
+    
+    @FXML
+    private Button autorizzazione;
+    @FXML
+    private Button stampa_voti;
+    @FXML
+    private Button stampa_lista_votanti;
+    @FXML
+    private Button stampa_lista_candidati;
+    @FXML
+    private Button cancella_tutto;
+    @FXML
+    private Button vota;
+    @FXML
+    private Button candidati;
+    
+    
+    @FXML
+    private void attiva(MouseEvent event){
+        stampa_lista_votanti.setDisable(false);
+        stampa_lista_candidati.setDisable(false);
+        cancella_tutto.setDisable(false);
+        stampa_voti.setDisable(false);
+    }
+    
     
 
     @Override
